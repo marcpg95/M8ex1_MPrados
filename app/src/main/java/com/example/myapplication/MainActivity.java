1668e10be7806e02db8958f6ba1806228743ae22
super <marcpg95@gmail.com>
2019-09-24 17:02:59 +0200

    first

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void hello(View Text){
+
+        Toast.makeText(this, "ejercico",Toast.LENGTH_LONG).show();
+    }
+}
